9d73ef9b48420c7cf85d4ef93d8c824d318c152a
5nuff <intechnic.l.r@gmail.com>
2019-12-09 09:26:57 -0500

    who-works-here

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import HelloWorld from '../components/HelloWorld'
+import AllEmployeePage from './AllEmployeePage'
 
 const HomePage = () => {
-  return <HelloWorld />
+  return (
+    <>
+      <h1 className="directory-title">Employee Directory</h1>
+      <AllEmployeePage />
+    </>
+  )
 }
-
 export default HomePage
